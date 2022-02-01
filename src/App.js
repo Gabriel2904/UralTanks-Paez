90db978da1a658712600066fed898c6cd68747ea
Gabriel2904 <gabriel2904@hotmail.com>
2022-01-31 23:58:28 -0300

    Counter

--- src/App.js
+++ src/App.js
@@ -1,38 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
-import * as React from 'react';
-import ReactDOM from 'react-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import * as React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import Cards from "./components/Cards/Cards";
 import Button from '@mui/material/Button';
-import Navbar from './components/Navbar/Navbar';
-import Header from './components/Header/Header';
-import Cards from './components/Cards/Cards'
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 66,
+      name: "t34",
+    };
+
+    //Binding
+    //this.handlerCounterUp = this.handlerCounterUp.bind(this);
+  }
+
+  handlerCounterUp = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  handlerCounterDown = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+  handlerCounterUpdateName = () => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+  //handlerCounterUp() {
+  //this.setState({counter: this.state.counter + 1})
+  //}
+
+  render() {
+    return (
+      <div className="App">
         <Navbar />
-        <Header title='title' subtitle='subtitle'/>
-        <div className='Cards'>
-        <Cards name='Historia' subtitle='loquesea' />
-        <Cards name='Actualidad' subtitle='loquesea'/>
-        <Cards name='Futuro' subtitle='loquesea'/>
+        <Header title="title" subtitle="subtitle" />
+        <div className="Cards">
+          <Cards name="Historia" subtitle="loquesea" />
+          <Cards name="Actualidad" subtitle="loquesea" />
+          <Cards name="Futuro" subtitle="loquesea" />
         </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <div className="CounterSection">
+          <p>Counter: {this.state.counter}</p>
+          <div className="btn.section">
+            <Button variant="contained" onClick={this.handlerCounterUp}>+</Button>
+            <Button variant="contained" onClick={this.handlerCounterDown}>-</Button>
+            
+          </div>
+          <h3>{this.state.name}</h3>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
